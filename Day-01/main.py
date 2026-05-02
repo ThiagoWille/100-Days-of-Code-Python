02586b2bd08f1ac3db57e7f1af7e3ecebe6b317d
Thiago Wille <113475340+ThiagoWille@users.noreply.github.com>
2026-05-02 17:49:11 -0300

    Add files via upload

---
+++ Day-01/main.py
@@ -0,0 +1,10 @@
+#Deafio do Dia1: Criar um Gerador de nome para Bandas!
+
+#Criando Variavéis
+
+city_name = input("Bem vindo ao gerador de nome de Bandas!\nQual o nome da cidade em que você nasceu?\n")
+pet_name = input("E qual o nome do seu Pet favorito?\n")
+
+#Usando a Variavél
+
+print(f"O nome da sua banda poderia ser:\n{city_name} {pet_name}")
